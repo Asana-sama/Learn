41fb396cca31460a3c09a36f875836358bb487c3
Asana-sama <69309320+Asana-sama@users.noreply.github.com>
2020-09-29 22:46:00 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,13 @@
+# Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные,
+# выведите на экран.
+
+name = 'Annah'
+age = 27
+print('Hello World, my name is', name, 'and I am', age, 'years old')
+print('And what about you?')
+
+name2 = input('What is your name?')
+age2 = input('And how old are you?')
+
+print('Nice yo meet you,', name2, '. I think,', age2, 'is an excellent age! You are lucky!')
